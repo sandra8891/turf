bac882028e5df3daef8f4838618acb24bb853580
sandra8891 <sandraks8891@gmail.com>
2025-05-24 15:06:07 +0530

    css

--- turf/turf/settings.py
+++ turf/turf/settings.py
@@ -131,7 +131,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sandraks8891@gmail.com'
-EMAIL_HOST_PASSWORD = 'gupz xzhw rzne ywtc'
+EMAIL_HOST_PASSWORD = 'hpum lpua hxfk datx'
 
 
 
